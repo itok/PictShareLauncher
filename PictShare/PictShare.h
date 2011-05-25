5d33cefd83734303a6acd9d898ebc1fe9449eaea
itok <i@itok.jp>
2011-05-26 03:57:45 +0900

    - add comments

--- PictShare/PictShare.h
+++ PictShare/PictShare.h
@@ -21,34 +21,40 @@
  * use pasteboard as data
  * - data:    image data
  * - uti:     data UTI (public.jpeg, public.png, etc.) <- see sample code
- * - name:    sender application name
- * - backURL: sender application callback url
+ * - options: key-val options (see PictShareParams.h)
+ *      - PS_PARAM_APPNAME : sender application name
+ *      - PS_PARAM_BACKURL : sender application callback url
+ *      - PS_PARAM_HASHTAG : twitter hashtag (PictShare v2.1)
  */
 +(BOOL) openPictShareWithData:(NSData*)data UTI:(NSString*)uti;
-+(BOOL) openPictShareWithData:(NSData*)data UTI:(NSString*)uti applicationName:(NSString*)name backURL:(NSString*)backURL;
 +(BOOL) openPictShareWithData:(NSData*)data UTI:(NSString*)uti options:(NSDictionary*)options;
++(BOOL) openPictShareWithData:(NSData*)data UTI:(NSString*)uti applicationName:(NSString*)name backURL:(NSString*)backURL; // deprecated
 
 /**
  * use pasteboard as image
  * - image:   image
  * # discard your photo's information such as exif
  * # PictShare use image as JPEG
- * - name:    sender application name
- * - backURL: sender application callback url
+ * - options: key-val options (see PictShareParams.h)
+ *      - PS_PARAM_APPNAME : sender application name
+ *      - PS_PARAM_BACKURL : sender application callback url
+ *      - PS_PARAM_HASHTAG : twitter hashtag (PictShare v2.1)
  */
 +(BOOL) openPictShareWithImage:(UIImage*)image;
-+(BOOL) openPictShareWithImage:(UIImage*)image applicationName:(NSString*)name backURL:(NSString*)backURL;
 +(BOOL) openPictShareWithImage:(UIImage*)image options:(NSDictionary*)options;
++(BOOL) openPictShareWithImage:(UIImage*)image applicationName:(NSString*)name backURL:(NSString*)backURL; // deprecated
 
 /**
  * use assets library
  * - urls:    assets urls (assets-library://xxx)
- * - name:    sender application name
- * - backURL: sender application callback url
+ * - options: key-val options (see PictShareParams.h)
+ *      - PS_PARAM_APPNAME : sender application name
+ *      - PS_PARAM_BACKURL : sender application callback url
+ *      - PS_PARAM_HASHTAG : twitter hashtag (PictShare v2.1)
  */
 +(BOOL) openPictShareWithAssetURLs:(NSArray*)urls;
-+(BOOL) openPictShareWithAssetURLs:(NSArray*)urls applicationName:(NSString*)name backURL:(NSString*)backURL;
 +(BOOL) openPictShareWithAssetURLs:(NSArray*)urls options:(NSDictionary*)options;
++(BOOL) openPictShareWithAssetURLs:(NSArray*)urls applicationName:(NSString*)name backURL:(NSString*)backURL; // deprecated
 
 /**
  * open AppStore
